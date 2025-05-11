addb63d913c6dc4ea7f78e280717c7c6e6e5b240
Arvind Venkatesan <arvindvenkat2529@gmail.com>
2025-05-11 11:24:27 -0500

    adding tail wind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
         secondary: '#151312',
         light : {
           100: '#D6C7FF',
-          200: 'A8B5DB',
+          200: '#A8B5DB',
           300: '#9CA4AB'
         },
         dark: {
